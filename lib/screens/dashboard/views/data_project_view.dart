9e344369489c93e860ef81c180193659f3963ad7
kim-eungseo <juniormoo87@gmail.com>
2022-04-26 02:26:28 +0900

    hingu_pingu

--- lib/screens/dashboard/views/data_project_view.dart
+++ lib/screens/dashboard/views/data_project_view.dart
@@ -1,35 +1,31 @@
 import 'package:admin/controllers/MenuController.dart';
 import 'package:admin/responsive.dart';
+import 'package:admin/viewmodels/data_project_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 
-// class Header extends StatelessWidget {
-//   const Header({
-//     Key? key,
-//   }) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       children: [
-//         if (!Responsive.isDesktop(context))
-//           IconButton(
-//             icon: Icon(Icons.menu),
-//             onPressed: context.read<MenuController>().controlMenu,
-//           ),
-//         if (!Responsive.isMobile(context))
-//           Text(
-//             "Auto-Cleansing Dashboard",
-//             style: Theme.of(context).textTheme.headline6,
-//           ),
-//         if (!Responsive.isMobile(context))
-//           Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
-//         SizedBox(width: defaultPadding),
-//         // Expanded(
-//         //   child: SearchField(),
-//         // ),
-//       ],
-//     );
-//   }
-// }
+class DataProjectView extends StatelessWidget {
+  late DataProjectViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    viewModel = Provider.of<DataProjectViewModel>(context); // Provider로 viewModel을 가져온다.
+    return Scaffold(
+      appBar: AppBar(title: Text('Post List')),
+      body: _buildPostList(),
+    );
+  }
+
+  Widget _buildPostList() {
+    final items = viewModel.dataList;     // viewModel에 저장된 items
+    final itemCount = items.length;
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final item = items[index];
+        return ListTile(title: Text(item.name));
+      },
+      itemCount: itemCount,
+    );
+  }
+}
--- lib/viewmodels/data_project_viewmodel.dart
+++ lib/viewmodels/data_project_viewmodel.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/cupertino.dart';
 
 class DataProjectViewModel with ChangeNotifier {
+  List<DataProjectDto> get dataList => _dataList;
+  List<DataProjectDto> _dataList = [];
 
-
+  DataProjectPreview get preview => _preview;
+  late DataProjectPreview _preview;
+  
 }
 
 class DataProjectDto {
+  late int id;
   String name;
   String author;
   String fileText;
@@ -13,3 +18,11 @@ class DataProjectDto {
   DataProjectDto(this.name, this.author, this.fileText);
 }
 
+class DataProjectPreview {
+  String name;
+  String author;
+  String timestamp;
+  Map head;
+
+  DataProjectPreview(this.name, this.author, this.timestamp, this.head);
+}
