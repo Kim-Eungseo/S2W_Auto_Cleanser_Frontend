3d69363a05e2c8d9a341ed44db060118c3a74892
kim-ngseo <juniormoo87@gmail.com>
2022-05-01 22:35:43 +0900

    get Data project by name 대성공

---
+++ lib/viewmodels/data_project_viewmodel.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data_project_viewmodel.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DataProjectDto _$DataProjectDtoFromJson(Map<String, dynamic> json) =>
+    DataProjectDto(
+      id: json['id'] as int?,
+      name: json['name'] as String?,
+      author: json['author'] as String?,
+      fileText: json['fileText'] as String?,
+      timestamp: json['timestamp'] as String?,
+      head: json['head'] as Map<String, dynamic>?,
+    );
+
+Map<String, dynamic> _$DataProjectDtoToJson(DataProjectDto instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'author': instance.author,
+      'fileText': instance.fileText,
+      'timestamp': instance.timestamp,
+      'head': instance.head,
+    };
